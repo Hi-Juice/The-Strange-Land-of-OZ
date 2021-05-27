a4e65b86771f1b24a7e7d554fe9978d9986b0160
Kkackit <rmssud03@never.com>
2021-05-27 23:52:28 +0900

    FirstCommit & TestCommit
    연동 테스트를 위한 커밋 입니다.
    Hi-Juice!

---
+++ New Unity ProjB/Assets/Scripts/Test_Scripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Scripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("유니티 깃허브 연동을 위한 테스트 스크립트입니다. Hi-Juice!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
